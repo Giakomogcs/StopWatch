75dd84fe65f1370084ed64b5eb46159f145368d1
Giovani <giakomogcs@gmail.com>
2023-07-25 14:47:30 -0300

    calculandoo tempo

--- Program.cs
+++ Program.cs
@@ -16,22 +16,41 @@ namespace StopWatch{
             Console.WriteLine("Quanto tempo deseja contar?");
 
             string data = Console.ReadLine().ToLower();
-            
-            if(int.Parse(data) == 0)
-                System.Environment.Exit(0);
 
-            char type = char.Parse(data.Substring(data.Length -1,1));
-            int time = int.Parse(data.Substring(0, data.Length -1));
+            try{
+                if(int.Parse(data) == 0){
+                    Console.Clear();
+                    System.Environment.Exit(0);
+                }
+            } catch{
 
-            int multiplicador = 1;
+                char type = char.Parse(data.Substring(data.Length -1,1));
+                int time = int.Parse(data.Substring(0, data.Length -1));
 
-            if (type == 'm')
-                multiplicador=60;
+                int multiplicador = 1;
 
-            if(time == 0)
-                System.Environment.Exit(0);
+                if (type == 'm')
+                    multiplicador=60;
 
-            Start(time*multiplicador);
+                if(time == 0)
+                    System.Environment.Exit(0);
+
+                PreStart(time*multiplicador);
+            }
+
+        }
+
+        static void PreStart(int time){
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Go...");
+            Thread.Sleep(2000);
+            Start(time);
         }
 
         static void Start(int time){
