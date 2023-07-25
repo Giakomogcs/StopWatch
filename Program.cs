043b38accb2568e2a05ca80031d07e1e402e15e7
Giovani <giakomogcs@gmail.com>
2023-07-25 14:33:35 -0300

    fluxo principal com menu

--- Program.cs
+++ Program.cs
@@ -16,9 +16,22 @@ namespace StopWatch{
             Console.WriteLine("Quanto tempo deseja contar?");
 
             string data = Console.ReadLine().ToLower();
-            Console.WriteLine(data);
+            
+            if(int.Parse(data) == 0)
+                System.Environment.Exit(0);
 
+            char type = char.Parse(data.Substring(data.Length -1,1));
+            int time = int.Parse(data.Substring(0, data.Length -1));
 
+            int multiplicador = 1;
+
+            if (type == 'm')
+                multiplicador=60;
+
+            if(time == 0)
+                System.Environment.Exit(0);
+
+            Start(time*multiplicador);
         }
 
         static void Start(int time){
@@ -34,6 +47,7 @@ namespace StopWatch{
             Console.Clear();
             Console.WriteLine("StopWatch finalizado!!");
             Thread.Sleep(2500);
+            Menu();
         }
     }
 }
